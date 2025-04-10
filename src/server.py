c1252b3a5f888d890ea8837ea0c10c9bbe3c3562
SudeeraDilshan <new1818155@gmail.com>
2025-04-10 14:28:28 +0530

    added get_consul_services

--- src/server.py
+++ src/server.py
@@ -4,6 +4,7 @@ from controllers.management import ManagementController
 from services.mqtt_service import MqttService
 import uvicorn
 from models.event import Event
+import requests
 
 app = FastAPI()
 
@@ -22,10 +23,27 @@ mqtt_service = MqttService()
 # Initialize Management Controller
 management_controller = ManagementController(mqtt_service)
 
+def fetch_services_data():
+    """
+    Fetch data from the Consul agent services endpoint.
+    """
+    url = "http://127.0.0.1:8500/v1/agent/services"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        return {"error": str(e)}
+
 # Define routes
 @app.post("/event/publish")
 async def publish_event(event_data: Event):
     return await management_controller.handle_event_publish(event_data)
+
+@app.get("/consul/services")
+async def get_consul_services():
+    return await management_controller.get_consul_services()
+
 
 if __name__ == "__main__":
     mqtt_service.connect()
