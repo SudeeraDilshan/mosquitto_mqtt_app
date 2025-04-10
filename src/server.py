cc3b93b98563564030ad5fc262679ac894ffa0f2
SudeeraDilshan <new1818155@gmail.com>
2025-04-10 13:33:29 +0530

    set event as parametres instead of dict

--- src/server.py
+++ src/server.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from controllers.management import ManagementController
 from services.mqtt_service import MqttService
 import uvicorn
+from models.event import Event
 
 app = FastAPI()
 
@@ -23,7 +24,7 @@ management_controller = ManagementController(mqtt_service)
 
 # Define routes
 @app.post("/event/publish")
-async def publish_event(event_data: dict):
+async def publish_event(event_data: Event):
     return await management_controller.handle_event_publish(event_data)
 
 if __name__ == "__main__":
